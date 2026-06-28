87db51dd41e284555cdece0b0ff9e45c1f348e78
BISCVITE30 <gonnki2004@gmail.com>
2026-06-28 21:25:05 +0200

    Version 2.0. Separate methods

--- numberGuessingGame.cs
+++ numberGuessingGame.cs
@@ -3,14 +3,24 @@ namespace Test
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Greeting()
         {
+
             Console.WriteLine("Welcome to the Number Guessing Game!!!");
             Console.Write("Please, select your number from 1 to 100: ");
             int guessNum = Convert.ToInt32(Console.ReadLine());
+            return guessNum;
+        }
+
+        static int GoRandom()
+        {
             Random rdNum = new Random();
             int rightNum = rdNum.Next(1, 101);
-
+            return rightNum;
+        }
+        
+        static void LetsGoGambling(int guessNum, int rightNum)
+        {
             while (guessNum != rightNum)
             {
                 if (guessNum < rightNum)
@@ -27,7 +37,15 @@ namespace Test
                 guessNum = Convert.ToInt32(Console.ReadLine());
             }
 
+            
             Console.WriteLine($"Nice! That’s the right number. This is {guessNum}");
+            return;
+        }
+        static void Main(string[] args)
+        {
+            int guessNum = Greeting();
+            int rightNum = GoRandom();
+            LetsGoGambling(guessNum, rightNum);
             return;
         }
     }
